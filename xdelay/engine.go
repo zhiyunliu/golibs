3adba288422eaa5ce2a71bb6cf7a9c8e7c8a953b
liujinyin <liujinyin@xunyou.com>
2022-04-11 03:41:49 -0700

    add the delay engine

---
+++ xdelay/engine.go
@@ -0,0 +1,100 @@
+package xdelay
+
+import (
+	"sync"
+	"time"
+
+	"github.com/zhiyunliu/golibs/xlist"
+)
+
+type DelayEngine struct {
+	slotCount int
+	//当前下标
+	curIndex uint
+	//环形槽
+	slots []*xlist.List
+	//关闭
+	closed   chan struct{}
+	onceLock sync.Once
+}
+
+//执行的任务函数
+type TaskCallback func(args ...interface{})
+
+//任务
+type dealyTask struct {
+	//循环次数
+	cycleCnt int
+	//执行的函数
+	callback TaskCallback
+	params   []interface{}
+}
+
+//创建一个延迟消息
+func NewDelayEngine(slotCount int) *DelayEngine {
+	dm := &DelayEngine{
+		slotCount: slotCount,
+		curIndex:  0,
+		closed:    make(chan struct{}),
+		slots:     make([]*xlist.List, slotCount),
+	}
+	for i := 0; i < slotCount; i++ {
+		dm.slots[i] = xlist.NewList()
+	}
+	return dm
+}
+
+//启动延迟消息
+func (e *DelayEngine) Start() {
+	go e.taskLoop()
+	<-e.closed
+}
+
+//关闭延迟消息
+func (e *DelayEngine) Close() {
+	e.onceLock.Do(func() {
+		close(e.closed)
+	})
+}
+
+//处理每1秒的任务
+func (e *DelayEngine) taskLoop() {
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-e.closed:
+			{
+				return
+			}
+		case <-ticker.C:
+			list := e.slots[e.curIndex]
+			if !list.IsEmpty() {
+				list.Iter(func(idx int, node *xlist.Node) bool {
+					task := node.Value.(*dealyTask)
+					if task.cycleCnt <= 0 {
+						go task.callback(task.params...)
+						node.Remove()
+						return true
+					}
+					task.cycleCnt--
+					return true
+				})
+
+			}
+			e.curIndex++
+			e.curIndex = e.curIndex % uint(e.slotCount)
+		}
+	}
+}
+
+//添加任务
+func (e *DelayEngine) AddTask(seconds uint, callback TaskCallback, params ...interface{}) error {
+	slotIdx := (e.curIndex + seconds) % uint(e.slotCount)
+	//把任务加入tasks中
+	e.slots[slotIdx].Append(&dealyTask{
+		cycleCnt: int(seconds / uint(e.slotCount)),
+		callback: callback,
+		params:   params,
+	})
+	return nil
+}
---
+++ xlist/list.go
@@ -0,0 +1,105 @@
+package xlist
+
+/*
+author:liujinyin
+date:2022-3-8
+desc:双休链表list
+*/
+
+import "fmt"
+
+type ListIterCallback func(idx int, node *Node) bool
+
+type List struct {
+	root   Node // 头节点
+	length int  // list长度
+}
+
+type Node struct {
+	list       *List
+	Value      interface{}
+	next, prev *Node
+}
+
+func (n *Node) Remove() {
+	n.prev.next = n.next
+	n.next.prev = n.prev
+	n.next = nil
+	n.prev = nil
+
+	n.list.length--
+	n.list = nil
+}
+
+func (n *Node) String() string {
+	return fmt.Sprint(n.Value)
+}
+
+func NewList() *List {
+	l := &List{} // 获取List{}的地址
+	l.length = 0 // list初始长度为0
+	l.root.next = &l.root
+	l.root.prev = &l.root
+	return l
+}
+
+func (n *List) String() string {
+	return fmt.Sprint(n.length)
+}
+
+func (l *List) IsEmpty() bool {
+	return l.root.next == &l.root
+}
+
+func (l *List) Length() int {
+	return l.length
+}
+
+func (l *List) Append(items ...interface{}) *List {
+
+	for _, item := range items {
+		var n *Node
+		switch v := item.(type) {
+		case Node:
+			n = &v
+		case *Node:
+			n = v
+		default:
+			n = &Node{Value: item}
+		}
+		n.list = l
+		n.next = &l.root
+		n.prev = l.root.prev
+		l.root.prev.next = n
+		l.root.prev = n
+		l.length++
+	}
+	return l
+}
+
+func (l *List) Remove(n *Node) {
+	n.prev.next = n.next
+	n.next.prev = n.prev
+	n.next = nil
+	n.prev = nil
+	l.length--
+}
+
+func (l *List) Iter(callback ListIterCallback) {
+	node := l.root.next
+	if l.IsEmpty() {
+		return
+	}
+	idx := 0
+	for {
+		next := node.next
+		if !callback(idx, node) {
+			return
+		}
+		if next == &l.root {
+			return
+		}
+		idx++
+		node = next
+	}
+}
