6b98030402acdd07a8130feb37662f1500a567b2
liujinyin <liujinyin@xunyou.com>
2022-04-14 22:19:12 -0700

    add the http content type map,it can be reset by custom.

--- httputil/http.go
+++ httputil/http.go
@@ -2,15 +2,30 @@ package httputil
 
 import (
 	"strings"
+	"sync"
 )
 
 const (
-	baseContentType = "application"
+	_baseContentType = "application"
 )
+
+var _subTypeMap sync.Map
+
+func init() {
+	_subTypeMap.Store("text", "text/plain;charset=utf-8")
+}
+
+func ResetContentType(subtype, fullType string) {
+	_subTypeMap.Store(subtype, fullType)
+}
 
 // ContentType returns the content-type with base prefix.
 func ContentType(subtype string) string {
-	return strings.Join([]string{baseContentType, subtype}, "/")
+	val, ok := _subTypeMap.Load(subtype)
+	if ok {
+		return val.(string)
+	}
+	return strings.Join([]string{_baseContentType, subtype}, "/")
 }
 
 // ContentSubtype returns the content-subtype for the given content-type.  The
@@ -19,6 +34,7 @@ func ContentType(subtype string) string {
 // according rfc7231.
 // contentType is assumed to be lowercase already.
 func ContentSubtype(contentType string) string {
+
 	left := strings.Index(contentType, "/")
 	if left == -1 {
 		return ""
