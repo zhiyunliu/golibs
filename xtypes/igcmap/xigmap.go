d079d031f3e81d377938ef60aa076fcf8fe0e1fc
zhiyunliu <360973986@qq.com>
2022-04-17 20:49:14 +0800

    增加一个忽略大小写的map 数据结构

---
+++ xtypes/igcmap/xigmap.go
@@ -0,0 +1,169 @@
+package igcmap
+
+import (
+	"reflect"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/zhiyunliu/golibs/xtypes"
+)
+
+/*
+igcmap = ignore case map
+忽略大小写的map 结构
+
+*/
+
+type Equalable interface {
+	Equal(interface{}) bool
+}
+
+type IgcMap struct {
+	data    xtypes.XMap
+	keyMap  map[string]string
+	rwmutex sync.RWMutex
+}
+
+func New(orignal map[string]interface{}) *IgcMap {
+	newOrgMap, keyMap := process(orignal)
+	m := &IgcMap{
+		data:   newOrgMap,
+		keyMap: keyMap,
+	}
+	return m
+}
+
+//Set 添加新值
+func (m *IgcMap) Set(key string, val interface{}) bool {
+	lowerkey := strings.ToLower(key)
+	m.rwmutex.Lock()
+	defer m.rwmutex.Unlock()
+
+	rk, ok := m.keyMap[lowerkey]
+	if ok && rk != key {
+		delete(m.data, rk)
+	}
+	m.data[key] = val
+	m.keyMap[lowerkey] = key
+	return ok
+}
+
+//Get 获取值
+func (m *IgcMap) Get(key string) (val interface{}, ok bool) {
+	lowerkey := strings.ToLower(key)
+	m.rwmutex.RLock()
+	defer m.rwmutex.RUnlock()
+	rk, ok := m.keyMap[lowerkey]
+	if !ok {
+		return nil, false
+	}
+	return m.data[rk], true
+}
+
+//MergeMap 合并Map
+func (m *IgcMap) MergeMap(other map[string]interface{}) {
+	for k, v := range other {
+		m.Set(k, v)
+	}
+}
+
+//MergeIgc 合并MergeIgc
+func (m *IgcMap) MergeIgc(other *IgcMap) {
+	for k, v := range other.data {
+		m.Set(k, v)
+	}
+}
+
+//Iter 迭代每一个字段元素
+func (m *IgcMap) Iter(callback func(key string, val interface{}) bool) {
+	for k, v := range m.data {
+		m.rwmutex.RLock()
+		if !callback(k, v) {
+			m.rwmutex.RUnlock()
+			break
+		}
+		m.rwmutex.RUnlock()
+	}
+}
+
+//Keys 获取所有的键
+func (m *IgcMap) Keys() []string {
+	m.rwmutex.RLock()
+	defer m.rwmutex.RUnlock()
+	return m.data.Keys()
+}
+
+//Del 删除一个元素
+func (m *IgcMap) Del(key string) {
+	lowerkey := strings.ToLower(key)
+	m.rwmutex.Lock()
+	defer m.rwmutex.Unlock()
+	rk, ok := m.keyMap[lowerkey]
+	if !ok {
+		return
+	}
+	delete(m.keyMap, lowerkey)
+	delete(m.data, rk)
+}
+
+//Orignal 返回Map 的浅拷贝副本
+func (m *IgcMap) Orignal() map[string]interface{} {
+	newVal := map[string]interface{}{}
+	for k, v := range m.data {
+		newVal[k] = v
+	}
+	return newVal
+}
+
+//Equal 判定两个对象是否相等
+func (m *IgcMap) Equal(o *IgcMap) bool {
+	if m == nil && o == nil {
+		return true
+	}
+	if m == nil || o == nil {
+		return false
+	}
+	if len(m.data) != len(o.data) {
+		return false
+	}
+
+	for k, v := range m.keyMap {
+		nk, ok := o.keyMap[k]
+		if !ok {
+			return false
+		}
+		if !reflect.DeepEqual(m.data[v], o.data[nk]) {
+			return false
+		}
+	}
+	return true
+}
+
+func process(orginal map[string]interface{}) (newMap map[string]interface{}, keyMap map[string]string) {
+	keyMap = make(map[string]string)
+	newMap = make(map[string]interface{})
+	if orginal == nil {
+		return
+	}
+	var (
+		ok       bool
+		mk       string
+		lowerkey string
+	)
+
+	xmapVal := xtypes.XMap(orginal)
+	keys := xmapVal.Keys()
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		lowerkey = strings.ToLower(k)
+		mk, ok = keyMap[lowerkey]
+		if ok {
+			delete(newMap, mk)
+		}
+		newMap[k] = orginal[k]
+		keyMap[lowerkey] = k
+	}
+	return
+}
---
+++ xtypes/igcmap/xigmap_test.go
@@ -0,0 +1,205 @@
+package igcmap
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name    string
+		orignal map[string]interface{}
+		want    *IgcMap
+	}{
+		{name: "1.无重复", orignal: map[string]interface{}{"a": 1, "b": "2"}, want: &IgcMap{data: map[string]interface{}{"a": 1, "b": "2"}}},
+		{name: "2.有重复,按key排序取后者", orignal: map[string]interface{}{"a": 1, "A": "2"}, want: &IgcMap{data: map[string]interface{}{"a": 1}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.orignal); !reflect.DeepEqual(got.data, tt.want.data) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIgcMap_Set(t *testing.T) {
+	type args struct {
+		key string
+		val interface{}
+	}
+	tests := []struct {
+		name   string
+		m      *IgcMap
+		args   args
+		keylen int
+		want   bool
+	}{
+		{name: "1.不存在", m: New(map[string]interface{}{}), args: args{key: "a", val: 1}, want: false, keylen: 1},
+		{name: "2.不存一致的key", m: New(map[string]interface{}{"a": 1}), args: args{key: "b", val: "2"}, want: false, keylen: 2},
+		{name: "3.存在相同的key", m: New(map[string]interface{}{"a": 1}), args: args{key: "a", val: 2}, want: true, keylen: 1},
+		{name: "4.存在大小写不一致的key", m: New(map[string]interface{}{"a": 1}), args: args{key: "A", val: "2"}, want: true, keylen: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.Set(tt.args.key, tt.args.val)
+			if got != tt.want {
+				t.Errorf("IgcMap.Set() = %v, want %v", got, tt.want)
+			}
+			if len(tt.m.keyMap) != tt.keylen {
+				t.Errorf("IgcMap.Set() KeyLen %v", len(tt.m.keyMap))
+			}
+		})
+	}
+}
+
+func TestIgcMap_Get(t *testing.T) {
+	tests := []struct {
+		name    string
+		m       *IgcMap
+		key     string
+		wantVal interface{}
+		wantOk  bool
+	}{
+		{name: "1.不存在的key", m: New(map[string]interface{}{}), key: "a", wantVal: nil, wantOk: false},
+		{name: "2.存在大小写不一致的key", m: New(map[string]interface{}{"A": 1}), key: "a", wantVal: 1, wantOk: true},
+		{name: "3.存在大小写一致的key", m: New(map[string]interface{}{"a": 1}), key: "a", wantVal: 1, wantOk: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVal, gotOk := tt.m.Get(tt.key)
+			if !reflect.DeepEqual(gotVal, tt.wantVal) {
+				t.Errorf("IgcMap.Get() gotVal = %v, want %v", gotVal, tt.wantVal)
+			}
+			if gotOk != tt.wantOk {
+				t.Errorf("IgcMap.Get() gotOk = %v, want %v", gotOk, tt.wantOk)
+			}
+		})
+	}
+}
+
+func TestIgcMap_MergeMap(t *testing.T) {
+	tests := []struct {
+		name   string
+		m      *IgcMap
+		args   map[string]interface{}
+		keyLen int
+	}{
+		{name: "1.空合并", m: New(map[string]interface{}{}), args: map[string]interface{}{}, keyLen: 0},
+		{name: "2.合并新值", m: New(map[string]interface{}{}), args: map[string]interface{}{"a": 1}, keyLen: 1},
+		{name: "3.覆盖为新值", m: New(map[string]interface{}{"a": 1}), args: map[string]interface{}{"a": 2}, keyLen: 1},
+		{name: "4.添加新值", m: New(map[string]interface{}{"a": 1}), args: map[string]interface{}{"b": 2}, keyLen: 2},
+		{name: "5.大小写不一致", m: New(map[string]interface{}{"a": 1}), args: map[string]interface{}{"A": 10}, keyLen: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.MergeMap(tt.args)
+			if len(tt.m.data) != tt.keyLen {
+				t.Errorf("MergeMap gotLen = %v, want %v", len(tt.m.data), tt.keyLen)
+			}
+		})
+	}
+}
+
+func TestIgcMap_MergeIgc(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		m      *IgcMap
+		args   *IgcMap
+		keyLen int
+	}{
+		{name: "1.空合并", m: New(map[string]interface{}{}), args: New(map[string]interface{}{}), keyLen: 0},
+		{name: "2.合并新值", m: New(map[string]interface{}{}), args: New(map[string]interface{}{"a": 1}), keyLen: 1},
+		{name: "3.覆盖为新值", m: New(map[string]interface{}{"a": 1}), args: New(map[string]interface{}{"a": 2}), keyLen: 1},
+		{name: "4.添加新值", m: New(map[string]interface{}{"a": 1}), args: New(map[string]interface{}{"b": 2}), keyLen: 2},
+		{name: "5.大小写不一致", m: New(map[string]interface{}{"a": 1}), args: New(map[string]interface{}{"A": 10}), keyLen: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.MergeIgc(tt.args)
+			if len(tt.m.data) != tt.keyLen {
+				t.Errorf("MergeMap gotLen = %v, want %v", len(tt.m.data), tt.keyLen)
+			}
+		})
+	}
+}
+
+func TestIgcMap_Iter(t *testing.T) {
+	tests := []struct {
+		name     string
+		m        *IgcMap
+		callback func(key string, val interface{}) bool
+	}{
+		{name: "1.", m: New(map[string]interface{}{"a": 1, "b": 2}), callback: func(key string, val interface{}) bool { return false }},
+		{name: "2.", m: New(map[string]interface{}{"a": 1, "b": 2}), callback: func(key string, val interface{}) bool { return true }},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.Iter(tt.callback)
+		})
+	}
+}
+
+func TestIgcMap_Keys(t *testing.T) {
+	tests := []struct {
+		name string
+		m    *IgcMap
+		want []string
+	}{
+		{name: "0.空map", m: New(map[string]interface{}{}), want: []string{}},
+		{name: "1.一个key", m: New(map[string]interface{}{"a": 1}), want: []string{"a"}},
+		{name: "2.多个key", m: New(map[string]interface{}{"a": 1, "b": 2}), want: []string{"a", "b"}},
+		{name: "3.大小写key", m: New(map[string]interface{}{"a": 1, "A": "2"}), want: []string{"a"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.Keys(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("IgcMap.Keys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIgcMap_Del(t *testing.T) {
+	tests := []struct {
+		name   string
+		m      *IgcMap
+		key    string
+		keyLen int
+	}{
+		{name: "1.一个不存在的key-a", m: New(map[string]interface{}{}), key: "a", keyLen: 0},
+		{name: "2.一个不存在的key-b", m: New(map[string]interface{}{"a": 1}), key: "b", keyLen: 1},
+		{name: "3.存在的key", m: New(map[string]interface{}{"a": 1}), key: "a", keyLen: 0},
+		{name: "4.大小写不一致", m: New(map[string]interface{}{"A": 1}), key: "a", keyLen: 0},
+		{name: "5.多个key", m: New(map[string]interface{}{"a": 1, "b": 2}), key: "a", keyLen: 1},
+		{name: "6.多个key-大小写", m: New(map[string]interface{}{"a": 1, "b": 2}), key: "A", keyLen: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.m.Del(tt.key)
+			if len(tt.m.data) != tt.keyLen {
+				t.Errorf("IgcMap.Del() = %v, want %v", len(tt.m.data), tt.keyLen)
+			}
+		})
+	}
+}
+
+func TestIgcMap_Orignal(t *testing.T) {
+	tests := []struct {
+		name string
+		m    *IgcMap
+		want map[string]interface{}
+	}{
+		{name: "1.空map", m: New(map[string]interface{}{}), want: map[string]interface{}{}},
+		{name: "2.存在数据", m: New(map[string]interface{}{"a": 1}), want: map[string]interface{}{"a": 1}},
+		{name: "3.存在多个数据", m: New(map[string]interface{}{"a": 1, "b": 2}), want: map[string]interface{}{"a": 1, "b": 2}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.Orignal(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("IgcMap.Orignal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
