01117611f8fa42b3c605f8e51fa2c79c93f4c9c4
liujinyin <liujinyin@xunyou.com>
2022-07-05 19:11:13 +0800

    增加map 的方法 getint,getInt64,getfloat64

--- xtypes/xmap.go
+++ xtypes/xmap.go
@@ -3,6 +3,8 @@ package xtypes
 import (
 	"encoding/json"
 	"fmt"
+	"math"
+	"reflect"
 	"strconv"
 
 	"github.com/zhiyunliu/golibs/xtransform"
@@ -85,4 +87,95 @@ func (m XMap) Translate(tpl string) string {
 func (m XMap) MarshalBinary() (data []byte, err error) {
 	tmp := map[string]interface{}(m)
 	return json.Marshal(tmp)
+}
+
+func (m XMap) GetInt(key string) (int, error) {
+	tmp, ok := m[key]
+	if !ok {
+		return 0, nil
+	}
+
+	switch val := tmp.(type) {
+	case int:
+		return val, nil
+	case int32:
+		return int(val), nil
+	case int64:
+		if math.MinInt <= val && val <= math.MaxInt {
+			return int(val), nil
+		}
+		return 0, fmt.Errorf("数据越界:int64=>int,%d", val)
+	case string:
+		return strToint(val)
+	default:
+		if strer, ok := tmp.(fmt.Stringer); ok {
+			return strToint(strer.String())
+		}
+	}
+
+	return 0, fmt.Errorf("不支持的数据类型:%s", reflect.TypeOf(tmp).Name())
+}
+
+func (m XMap) GetInt64(key string) (int64, error) {
+	tmp, ok := m[key]
+	if !ok {
+		return 0, nil
+	}
+
+	switch val := tmp.(type) {
+	case int:
+		return int64(val), nil
+	case int32:
+		return int64(val), nil
+	case int64:
+		return val, nil
+	case string:
+		return strToint64(val)
+	default:
+		if strer, ok := tmp.(fmt.Stringer); ok {
+			return strToint64(strer.String())
+		}
+	}
+
+	return 0, fmt.Errorf("不支持的数据类型:%s", reflect.TypeOf(tmp).Name())
+}
+
+func (m XMap) GetFloat64(key string) (float64, error) {
+	tmp, ok := m[key]
+	if !ok {
+		return 0, nil
+	}
+	switch val := tmp.(type) {
+	case float32:
+		return float64(val), nil
+	case float64:
+		return float64(val), nil
+	case string:
+		return strTofloat64(val)
+	default:
+		if strer, ok := tmp.(fmt.Stringer); ok {
+			return strTofloat64(strer.String())
+		}
+	}
+
+	return 0, fmt.Errorf("不支持的数据类型:%s", reflect.TypeOf(tmp).Name())
+}
+
+func strToint(str string) (int, error) {
+	var t64 int64
+	t64, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if math.MinInt <= t64 && t64 <= math.MaxInt {
+		return int(t64), nil
+	}
+	return 0, fmt.Errorf("数据越界:int64=>int,%d", t64)
+}
+
+func strToint64(str string) (int64, error) {
+	return strconv.ParseInt(str, 10, 64)
+}
+func strTofloat64(str string) (float64, error) {
+	return strconv.ParseFloat(str, 64)
 }
