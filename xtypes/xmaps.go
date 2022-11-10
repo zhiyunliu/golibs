fccac25132ba95df113be1239d207f976162fc66
liujinyin <liujinyin@xunyou.com>
2022-11-10 16:12:45 +0800

    增加xtypes.XMaps 的Scan 方法

--- xtypes/xmaps.go
+++ xtypes/xmaps.go
@@ -1,5 +1,11 @@
 package xtypes
 
+import (
+	"reflect"
+
+	"github.com/zhiyunliu/golibs/xtypes/internal"
+)
+
 type XMaps []XMap
 
 func (ms *XMaps) Append(i ...XMap) XMaps {
@@ -23,4 +29,42 @@ func (ms XMaps) Get(idx int) XMap {
 		return ms[idx]
 	}
 	return map[string]interface{}{}
+}
+
+func (ms XMaps) Scan(obj interface{}) error {
+	rv := reflect.ValueOf(obj)
+	if rv.Kind() != reflect.Pointer || rv.IsNil() {
+		return &internal.XMapScanError{Type: reflect.TypeOf(obj)}
+	}
+
+	sliceType := rv.Elem().Type()
+
+	if !(sliceType.Kind() == reflect.Slice) {
+		return &internal.XMapScanError{Message: "Not Slice"}
+	}
+	structType := sliceType.Elem()
+	var subIsPtr bool = false
+	if structType.Kind() == reflect.Ptr {
+		subIsPtr = true
+		structType = structType.Elem()
+	}
+
+	newv := reflect.MakeSlice(sliceType, len(ms), len(ms))
+	for i := range ms {
+
+		tmp := reflect.New(structType)
+		err := ms[i].Scan(tmp.Interface())
+		if err != nil {
+			return err
+		}
+		itemV := tmp
+		if !subIsPtr {
+			itemV = itemV.Elem()
+		}
+		newv.Index(i).Set(itemV)
+	}
+
+	//reflect.Copy(rv.Elem(), newv)
+	rv.Elem().Set(newv)
+	return nil
 }
