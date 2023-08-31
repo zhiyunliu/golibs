55f686b3f30f4f0ca6d19a3e62b1b594a078c8c7
liujinyin <liujinyin@xunyou.com>
2023-08-31 16:44:42 +0800

    增加对地址获取方法的重构

--- xnet/addr.go
+++ xnet/addr.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -19,49 +20,77 @@ var (
 		"rand": randPortPicker,
 		"seq":  seqPortPicker,
 	}
+
+	_portRegexpPattern1 = regexp.MustCompile(`^:(\d+)$`)
+	_portRegexpPattern2 = regexp.MustCompile(`^\[(\d+),(\d+)\)(\:rand)?$`)
+	_portRegexpPattern3 = regexp.MustCompile(`^\[(\d+),(\d+)\)\:(seq|rand)\:(\d+)$`)
+
+	_portRegexpList []*addrRegexp = []*addrRegexp{
+		{
+			regexp: _portRegexpPattern1,
+			buildAddr: func(logger xlog.Logger, localIp string, subList []string) (string, error) {
+				return fmt.Sprintf("%s:%s", localIp, subList[0]), nil
+			},
+		},
+		{
+			regexp: _portRegexpPattern2,
+			buildAddr: func(logger xlog.Logger, localIp string, subList []string) (string, error) {
+				begin, err := strconv.ParseInt(subList[0], 10, 32)
+				if err != nil {
+					return "", err
+				}
+				end, err := strconv.ParseInt(subList[1], 10, 32)
+				if err != nil {
+					return "", err
+				}
+				nport := randPortPicker(logger, localIp, begin, end, 1)
+				return fmt.Sprintf("%s:%d", localIp, nport), nil
+			},
+		},
+		{
+			regexp: _portRegexpPattern3,
+			buildAddr: func(logger xlog.Logger, localIp string, subList []string) (string, error) {
+				begin, err := strconv.ParseInt(subList[0], 10, 32)
+				if err != nil {
+					return "", err
+				}
+				end, err := strconv.ParseInt(subList[1], 10, 32)
+				if err != nil {
+					return "", err
+				}
+				pickmethod := subList[2]
+				pickmethod = strings.Trim(pickmethod, ":")
+
+				step, err := strconv.ParseInt(subList[3], 10, 32)
+				if err != nil {
+					return "", err
+				}
+				call, ok := portPick[pickmethod]
+				if !ok {
+					return "", fmt.Errorf("指定端口配置错误")
+				}
+				nport := call(logger, localIp, begin, end, step)
+				if nport == 0 {
+					return "", fmt.Errorf("未获取到有效的端口,请检查配置")
+				}
+				return fmt.Sprintf("%s:%d", localIp, nport), nil
+			},
+		},
+	}
 )
 
 // :8080
-// 2000-30000
-// 2000-30000:rand
-// 2000-30000:seq:2
+// [2000,30000)
+// [2000,30000):rand
+// [2000,30000):seq:5
 func GetAvaliableAddr(logger xlog.Logger, localIp string, addr string) (newAddr string, err error) {
-	//没有指定范围
-	if !strings.Contains(addr, "-") {
-		return addr, nil
+	for _, reg := range _portRegexpList {
+		if !reg.MatchString(addr) {
+			continue
+		}
+		return reg.BuildAddr(logger, localIp, addr)
 	}
-	method := "rand"
-	step := int64(1)
-	parties := strings.SplitN(addr, ":", 3)
-	if len(parties) == 2 {
-		addr = parties[0]
-		method = parties[1]
-	}
-	if len(parties) == 3 {
-		addr = parties[0]
-		method = parties[1]
-		step, _ = strconv.ParseInt(parties[2], 10, 32)
-	}
-
-	parties = strings.SplitN(addr, "-", 2)
-	begin, err := strconv.ParseInt(parties[0], 10, 32)
-	if err != nil || begin <= 0 {
-		return "", fmt.Errorf("指定端口配置错误:%s", addr)
-	}
-	end, err := strconv.ParseInt(parties[1], 10, 32)
-	if err != nil || end <= 0 || end < begin {
-		return "", fmt.Errorf("指定端口配置错误:%s", addr)
-	}
-	call, ok := portPick[method]
-	if !ok {
-		return "", fmt.Errorf("指定端口配置错误:%s", addr)
-	}
-	np := call(logger, localIp, begin, end, step)
-	if np == 0 {
-		return "", fmt.Errorf("未获取到有效的端口,请检查配置:%s", addr)
-	}
-	newAddr = fmt.Sprintf("%s:%d", localIp, np)
-	return
+	return "", fmt.Errorf("指定端口配置错误:%s", addr)
 }
 
 func randPortPicker(logger xlog.Logger, localIp string, begin, end, step int64) int64 {
@@ -93,4 +122,21 @@ func ScanPort(protocol string, hostname string, port int64) bool {
 	}
 	defer conn.Close()
 	return true
+}
+
+type addrRegexp struct {
+	regexp    *regexp.Regexp
+	buildAddr func(logger xlog.Logger, localIp string, subList []string) (string, error)
+}
+
+func (p addrRegexp) MatchString(addr string) bool {
+	return p.regexp.MatchString(addr)
+}
+func (p addrRegexp) BuildAddr(logger xlog.Logger, localIp string, addr string) (newAddr string, err error) {
+	sublist := p.regexp.FindStringSubmatch(addr)
+	newAddr, err = p.buildAddr(logger, localIp, sublist[1:])
+	if err != nil {
+		return newAddr, fmt.Errorf("%w:%s", err, addr)
+	}
+	return newAddr, nil
 }
---
+++ xnet/addr_test.go
@@ -0,0 +1,68 @@
+package xnet
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/zhiyunliu/golibs/xlog"
+)
+
+type testLogger struct {
+}
+
+func (t testLogger) Name() string {
+	return "test"
+}
+func (t testLogger) SessionID() string {
+	return "testid"
+}
+func (t testLogger) Log(level xlog.Level, args ...interface{}) {
+
+}
+func (t testLogger) Logf(level xlog.Level, format string, args ...interface{}) {
+
+}
+func (t testLogger) Close() {
+
+}
+
+func TestGetAvaliableAddr(t *testing.T) {
+	logger := &testLogger{}
+	localIp := "127.0.0.1"
+	var min int64 = 1000
+	var max int64 = 2000
+	tests := []struct {
+		name        string
+		addr        string
+		min         int64
+		max         int64
+		wantNewAddr string
+		wantErr     bool
+	}{
+		{name: "1.", addr: ":1001", wantNewAddr: "127.0.0.1:1001", wantErr: false},
+		{name: "2.", addr: "[1000,2000)", wantNewAddr: "", wantErr: false},
+		{name: "3.", addr: "[1000,2000):rand", wantNewAddr: "", wantErr: false},
+		{name: "4.", addr: "[1000,2000):seq:5", wantNewAddr: "", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotNewAddr, err := GetAvaliableAddr(logger, localIp, tt.addr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAvaliableAddr() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			parties := strings.Split(gotNewAddr, ":")
+			nport, err := strconv.ParseInt(parties[1], 10, 64)
+			if err != nil {
+				t.Error(err)
+			}
+			if !(min <= nport && nport < max) {
+				t.Error(err)
+			}
+			if tt.wantNewAddr != "" && gotNewAddr != tt.wantNewAddr {
+				t.Errorf("GetAvaliableAddr() = %v, want %v", gotNewAddr, tt.wantNewAddr)
+			}
+		})
+	}
+}
