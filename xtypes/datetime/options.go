aedd8c5618dd8ede350a915ccad0505d266bfd8e
liujinyin <liujinyin@xunyou.com>
2026-01-26 18:37:41 +0800

    datetime增加WithFormat 的Option

--- xtypes/datetime/options.go
+++ xtypes/datetime/options.go
@@ -7,3 +7,10 @@ type Option func(*options)
 type options struct {
 	Format string
 }
+
+// WithFormat
+func WithFormat(format string) Option {
+	return func(o *options) {
+		o.Format = format
+	}
+}
