bf00a2c92b521043b0f03d80e201a3102c8717e3
liujinyin <liujinyin@xunyou.com>
2026-08-06 17:05:29 +0800

    增加数据的EventId的Getter接口定义

--- xsse/sse.go
+++ xsse/sse.go
@@ -1,6 +1,26 @@
 package xsse
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+// EventIdGetter interface
+type EventIdGetter interface {
+	GetEventId() string
+}
+
+var eventIdGetterType = reflect.TypeOf((*EventIdGetter)(nil)).Elem()
+
+// needCheckEventIdGetter 判定类型 T 是否可能实现 EventIdGetter。
+// T 为接口类型时，实际类型只能在运行时确定，需逐个元素断言。
+func checkEventIdGetter[T any]() bool {
+	typ := reflect.TypeFor[T]()
+	if typ.Kind() == reflect.Interface {
+		return true
+	}
+	return typ.Implements(eventIdGetterType)
+}
 
 // ServerSentEvents interface
 type ServerSentEvents interface {
@@ -9,14 +29,17 @@ type ServerSentEvents interface {
 
 // ResultChan is a channel that returns events
 type ResultChan[T any] struct {
-	dataChan chan T
-	idx      int
+	dataChan    chan T
+	idx         int
+	hasIdGetter bool
 }
 
 // NewResultChan creates a new ResultChan
 func NewResultChan[T any](dataChan chan T) *ResultChan[T] {
+
 	return &ResultChan[T]{
-		dataChan: dataChan,
+		dataChan:    dataChan,
+		hasIdGetter: checkEventIdGetter[T](),
 	}
 }
 
@@ -27,22 +50,34 @@ func (s *ResultChan[T]) GetEvent() (evt *Event, ok bool) {
 		return nil, false
 	}
 	s.idx++
+
+	var id string
+	if s.hasIdGetter {
+		if getter, ok := any(item).(EventIdGetter); ok {
+			id = getter.GetEventId()
+		}
+	} else {
+		id = fmt.Sprint(s.idx)
+	}
+
 	return &Event{
-		Id:   fmt.Sprint(s.idx),
+		Id:   id,
 		Data: item,
 	}, ok
 }
 
 // ResultList is a list that returns events
 type ResultList[T any] struct {
-	dataList []T
-	idx      int
+	dataList    []T
+	idx         int
+	hasIdGetter bool
 }
 
-// NewResultChan creates a new ResultChan
+// NewResultList creates a new ResultList
 func NewResultList[T any](dataList ...T) *ResultList[T] {
 	return &ResultList[T]{
-		dataList: dataList,
+		dataList:    dataList,
+		hasIdGetter: checkEventIdGetter[T](),
 	}
 }
 
@@ -53,8 +88,18 @@ func (s *ResultList[T]) GetEvent() (evt *Event, ok bool) {
 	}
 	item := s.dataList[s.idx]
 	s.idx++
+
+	var id string
+	if s.hasIdGetter {
+		if getter, ok := any(item).(EventIdGetter); ok {
+			id = getter.GetEventId()
+		}
+	} else {
+		id = fmt.Sprint(s.idx)
+	}
+
 	return &Event{
-		Id:   fmt.Sprint(s.idx),
+		Id:   id,
 		Data: item,
 	}, true
 }
